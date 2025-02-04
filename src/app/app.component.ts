901054d1a6d4a565fb1f5c0cc5a749ebea4ec4ec
Louison Savarese <louison.savarese@gmail.com>
2025-02-04 11:22:39 -0500

    navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
